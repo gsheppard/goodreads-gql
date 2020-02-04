791e08a78cebb5b833f589bd3e84c8f9d7e518fe
Gregory Sheppard <gsheppard@xogrp.com>
2020-02-04 14:18:39 -0600

    📝 Add a lot of comments explaining core concepts of GraphQL and Apollo

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,18 +1,44 @@
-// GS: These are kinda like Controllers
+/* - Glossery -
+  Resolver:
+  A function that connects schema fields and types to various backends.
+  Resolvers provide the instructions for turning a GraphQL operation into data.
+  It can retrieve data from or write data to anywhere, including a SQL, No-SQL,
+  or graph database, a micro-service, and a REST API. Resolvers can also return
+  strings, ints, null, and other primitives.
+*/
 const { authors, books } = require('./utils/sampleData');
 
 const resolvers = {
   Query: {
+    /*
+      The `Query.author` resolver directly correlates to `Query.author` in our schema
+    */
     author: async (parent, args, context, info) => {
-      console.log('Query.author:', author, args, context, info)
+      /*
+        parent: An object that contains the result returned from the resolver on the parent type.
+          In this case, because `Author` is a top-level schema, `parent` is undefined.
+        args: The arguments passed in from the Query, as defined in the schema.
+        context: An object shared by all resolvers in a GraphQL operation. We use the context to contain per-request state such as authentication information and access our data sources.
+        info: Information about the execution state of the operation which should only be used in advanced cases
+      */
       return authors.find((a => a.id === args.id));
     },
   },
 
   Author: {
-    books: async (author, args, context, info) => {
-      console.log('Author.book:', author, args, context, info)
-      return books.filter(b => b.authorId === author.id);
+    /*
+      `Author.books` directly correlates to how Apollo should resolve when
+      querying for `Author.books` from the schema. If `books` is not on the
+      original data set, then Apollo will just return `null`.
+    */
+    books: async (parent, args, context, info) => {
+      /*
+        parent: In this case, `parent` is the `author` data.
+
+        This could simply be rewritten as the following since we don't need the extra args:
+          books: async (author) => {
+      */
+      return books.filter(b => b.authorId === parent.id);
     },
   },
 };
--- src/schema.js
+++ src/schema.js
@@ -1,17 +1,58 @@
+/* - Glossery -
+  Schema:
+  A GraphQL schema is at the center of any GraphQL server implementation and
+  describes the functionality available to the clients which connect to it.
+
+  Scalar Type:
+  A type that qualifies the data a GraphQL field resolves. GraphQL ships with
+  some scalar types out of the box; Int, Float, String, Boolean and ID. However,
+  a custom scalar type such as Date can be specified in a GraphQL service
+  implementation.
+*/
 const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-  type Book {
-    title: String
-    author: Author
-  }
-
+  """
+    Schemas for data
+  """
   type Author {
     id: String
     name: String
     books: [Book]
   }
 
+  type Book {
+    id: String
+    title: String
+    authorId: String
+  }
+
+  """
+    - Glossery -
+    Query:
+    A read-only fetch operation to request data from a GraphQL service.
+
+    All Query definitions go here. For example, a query for
+      'author' would look like this:
+
+    query GetAuthorById {
+      author(id: 'author-111') {
+        name
+      }
+    }
+
+    Because our Author schema has 'books', which points to an array of 'Book',
+    we can also query for the author's books:
+
+    query GetAuthorById {
+      author(id: 'author-111') {
+        name
+        books {
+          title
+        }
+      }
+    }
+  """
   type Query {
     author(id: ID!): Author
   }
